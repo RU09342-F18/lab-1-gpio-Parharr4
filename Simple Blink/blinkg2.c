949ca01a02abaca0c74b526f4c7fb9211691f7a9
Parharr4 <35544325+Parharr4@users.noreply.github.com>
2018-09-21 22:32:42 -0400

    Update blinkg2.c

--- Simple Blink/blinkg2.c
+++ Simple Blink/blinkg2.c
@@ -1,19 +1,19 @@
-#include <msp430.h>				
+#include <msp430.h> 
 
 
 /**
- * blink.c
+ * main.c
  */
-void main(void)
+int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= 0x01;					// configure P1.0 as output
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+        P1SEL &= ~0x00;
+        P1DIR |= 0x00;
 
-	volatile unsigned int i;		// volatile to prevent optimization
-
-	while(1)
-	{
-		P1OUT ^= 0x01;				// toggle P1.0
-		for(i=10000; i>0; i--);     // delay
-	}
+     while(1)
+     {
+        P1OUT ^= 0x00;
+        __delay_cycles(1000000);
+     }
+	return 0;
 }
